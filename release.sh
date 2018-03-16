d7e7c74dddba77214896c5351956a25050325835
Thomas Kuchs <thomas.kuchs@xcom.de>
2018-03-16 12:26:52 +0100

    build: Set new release version

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CI_BUILD_TAG=goxc-0.1.10
+export CI_BUILD_TAG=goxc-0.1.11
 export CI_BUILD_REF=`git log --decorate | head -n 1 | awk -F' ' '{print $2}'`
 export BUILD_STAMP=`date -u +%Y/%m/%d\_%H:%M:%S\_%Z` # -ldflags -X doesn't like spaces
 export BUILD_NUMBER=`echo $BUILD_STAMP | sed -e 's/^..//g' | sed -e 's/....$//g' | sed -e 's/\//./g' | sed -e 's/_/-/g' | sed -e 's/://g'`
